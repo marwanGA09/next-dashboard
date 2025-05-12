25b9afe57d890d960095f4e6f20d37d1f8f23948
Adem Kedir <ademkedir724@gmail.com>
2025-05-12 23:16:48 +0300

    add metadata

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,9 +1,12 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
-import { promise } from 'zod';
 import { notFound } from 'next/navigation';
+import { Metadata } from 'next';
 
+export const metadata: Metadata = {
+  title: 'Edit Invoice',
+};
 export default async function Page({
   params,
 }: {
